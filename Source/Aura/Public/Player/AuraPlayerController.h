413937763f08c1e6b06889421ca88fe36503ce8b
sachiko morimoto <morimoto.sachiko@gmail.com>
2024-07-13 16:42:52 +0900

    14. Post Process Highlight まで終了

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -9,6 +9,7 @@
 class UInputMappingContext;
 class UInputAction;
 struct FInputActionValue;
+class IEnemyInterface;
 
 /**
  * 
@@ -20,6 +21,8 @@ class AURA_API AAuraPlayerController : public APlayerController
 
 public:
 	AAuraPlayerController();
+	virtual void PlayerTick(float DeltaTime) override;
+
 
 private:
 	virtual void BeginPlay() override;
@@ -33,4 +36,13 @@ private:
 	TObjectPtr<UInputAction> MoveAction;
 
 	void Move(const FInputActionValue& InputActionValue);
+
+	void CursorTrace();
+
+	//TScriptInterface<IEnemyInterFace> LastActor;
+	//TScriptInterface<IEnemyInterFace> ThisActor;
+	IEnemyInterface* LastActor;
+	IEnemyInterface* ThisActor;
+
 };
+
